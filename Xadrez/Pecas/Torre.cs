cfb72cf4fbc35543c86d0061b6504825e1ad4f72
Carlos Oliveira <Oliveira_carlos2017@outlook.com.br>
2019-05-21 20:20:37 -0300

    Movimentos da torre feito

--- Xadrez/Pecas/Torre.cs
+++ Xadrez/Pecas/Torre.cs
@@ -6,8 +6,68 @@ namespace Xadrez {
 
         }
         public override bool[,] movimentosPossiveis(){
-            
-            return null;
+            bool validator=true;
+            bool[,] mat=new bool[tab.linhas,tab.colunas];
+            Posicao pos= new Posicao(posicao.Linha,posicao.Coluna);
+            //East
+            while(validator==true){
+                pos.definirvalores(pos.Linha,pos.Coluna-1);
+                if(tab.posicaovalida(pos)&&podeMover(pos)){
+                    mat[pos.Linha,pos.Coluna]=true;
+                }
+                else if(tab.posicaovalida(pos)==true&&podeMover(pos)==false){
+                    validator=false;
+                }
+                else{
+                    validator=false;
+                }
+            }
+            validator=true;
+            //WEST
+            pos.definirvalores(posicao.Linha,posicao.Coluna);
+            while(validator==true){
+                pos.definirvalores(pos.Linha,pos.Coluna+1);
+                if(tab.posicaovalida(pos)&&podeMover(pos)){
+                    mat[pos.Linha,pos.Coluna]=true;
+                }
+                else if(tab.posicaovalida(pos)==true&&podeMover(pos)==false){
+                    validator=false;
+                }
+                else{
+                    validator=false;
+                }
+            }
+            //North
+            validator=true;
+            pos.definirvalores(posicao.Linha,posicao.Coluna);
+            while(validator==true){
+                pos.definirvalores(pos.Linha-1,pos.Coluna);
+                if(tab.posicaovalida(pos)&&podeMover(pos)){
+                    mat[pos.Linha,pos.Coluna]=true;
+                }
+                else if(tab.posicaovalida(pos)==true&&podeMover(pos)==false){
+                    validator=false;
+                }
+                else{
+                    validator=false;
+                }
+            }
+            //South
+            validator=true;
+            pos.definirvalores(posicao.Linha,posicao.Coluna);
+            while(validator==true){
+                pos.definirvalores(pos.Linha+1,pos.Coluna);
+                if(tab.posicaovalida(pos)&&podeMover(pos)){
+                    mat[pos.Linha,pos.Coluna]=true;
+                }
+                else if(tab.posicaovalida(pos)==true&&podeMover(pos)==false){
+                    validator=false;
+                }
+                else{
+                    validator=false;
+                }
+            }
+            return mat;
         }
         public override string ToString() {
             return "T";
