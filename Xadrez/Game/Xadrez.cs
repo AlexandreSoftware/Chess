87ce43b398d11f26f0187cbdaf65f58389bf3cd5
Carlos Oliveira <Oliveira_carlos2017@outlook.com.br>
2019-05-21 13:56:30 -0300

    melhorando logica de movimentação, nao e possivel movimentação para pecas da mesma cor, e o bug de movimentaçao em celulas vazias foi resolvido

--- Xadrez/Game/Xadrez.cs
+++ Xadrez/Game/Xadrez.cs
@@ -5,43 +5,59 @@ using System.Collections.Generic;
 namespace Xadrez {
     class Xadrez : Game {
         public override void GameLoop() {
-            bool gameisfinished = false;
+            bool gameIsFinished = false;
             PosicaoXadrez init;
-            List<Peca> capturadosbrancos = new List<Peca>();
-            List<Peca> capturadospretos = new List<Peca>();
+            List<Peca> capturadosBrancos = new List<Peca>();
+            List<Peca> capturadosPretos = new List<Peca>();
             Tabuleiro tab = new Tabuleiro(8, 8);
             MontarTabuleiro(tab);
             Console.Clear();
-            while (!gameisfinished) {
-                Tela.imprimirxadrez(tab);
-                foreach (Peca item in capturadosbrancos) {
-                    Console.Write(item);
-                }
-                foreach (Peca item in capturadospretos) {
-                    Console.Write(item);
-                }
+            while (!gameIsFinished) {
                 
+                Tela.imprimirxadrez(tab);
+                Console.Write("PECAS Brancas:[");
+                foreach (Peca item in capturadosBrancos) {
+                    Console.Write(item+" ");
+                }
+                ConsoleColor consoleColor=Console.ForegroundColor;
+                Console.Write("]\n");
+                Console.ForegroundColor=ConsoleColor.Yellow;
+                Console.Write("Pecas Pretas:[");
+                foreach (Peca item in capturadosPretos) {
+                    Console.Write(item+" ");
+                }
+                System.Console.Write("]");
+                Console.ForegroundColor=consoleColor;
+                System.Console.WriteLine();
+                try{
+                System.Console.Write("Origem: ");
                 init = Tela.lerPosicaoXadrez();
-                Peca p1 = tab.peca(init.ToPosicao());
-                Console.Write("\r");
                 if (tab.posicaovalida(init.ToPosicao()) && tab.existepeca(init.ToPosicao())) {
-                    
-                    PosicaoXadrez target = Tela.lerPosicaoXadrez();
+                    Peca p1 = tab.peca(init.ToPosicao());
+                    System.Console.Write("Destino: ");
+                    PosicaoXadrez destino = Tela.lerPosicaoXadrez();
 
-                    Peca p2 = tab.MoverPeca(p1, init.ToPosicao(), target.ToPosicao());
-                    if (tab.posicaovalida(target.ToPosicao()) && tab.existepeca(target.ToPosicao())) {
+                    Peca p2 = tab.MoverPeca(init.ToPosicao(), destino.ToPosicao());
+                    if (p2!=null) {
 
                         Cor p2cor = p2.cor;
                         if (p2cor == Cor.Branco) {
-                            capturadosbrancos.Add(p2);
+                            capturadosBrancos.Add(p2);
                         }
                         else {
-                            capturadospretos.Add(p2);
+                            capturadosPretos.Add(p2);
                         }
                     }
-                    else {
+                    
+                }
+                else {
                         Console.WriteLine("input invalido, tente novamente");
-                    }
+                        Console.ReadKey();
+                }
+                }
+                catch(TabuleiroException e){
+                        System.Console.WriteLine(e.Message);
+                        Console.ReadKey();
                 }
             }
         }
@@ -49,27 +65,27 @@ namespace Xadrez {
     public static void MontarTabuleiro(Tabuleiro tab) {
         Tela.imprimirtabuleiro(tab);
         for (char i = 'a'; i < 'h' + 1; i++) {
-            tab.Colocarpeca(new Peao(tab, Cor.Branco), new PosicaoXadrez(i, 2).ToPosicao());
-            tab.Colocarpeca(new Peao(tab, Cor.Preto), new PosicaoXadrez(i, 7).ToPosicao());
+            tab.colocarPeca(new Peao(tab, Cor.Branco), new PosicaoXadrez(i, 2).ToPosicao());
+            tab.colocarPeca(new Peao(tab, Cor.Preto), new PosicaoXadrez(i, 7).ToPosicao());
         }
         //PRETAS
-        tab.Colocarpeca(new Torre(tab, Cor.Preto), new PosicaoXadrez('a', 8).ToPosicao());
-        tab.Colocarpeca(new Cavalo(tab, Cor.Preto), new PosicaoXadrez('b', 8).ToPosicao());
-        tab.Colocarpeca(new Bispo(tab, Cor.Preto), new PosicaoXadrez('c', 8).ToPosicao());
-        tab.Colocarpeca(new Rainha(tab, Cor.Preto), new PosicaoXadrez('d', 8).ToPosicao());
-        tab.Colocarpeca(new Rei(tab, Cor.Preto), new PosicaoXadrez('e', 8).ToPosicao());
-        tab.Colocarpeca(new Bispo(tab, Cor.Preto), new PosicaoXadrez('f', 8).ToPosicao());
-        tab.Colocarpeca(new Cavalo(tab, Cor.Preto), new PosicaoXadrez('g', 8).ToPosicao());
-        tab.Colocarpeca(new Torre(tab, Cor.Preto), new PosicaoXadrez('h', 8).ToPosicao());
+        tab.colocarPeca(new Torre(tab, Cor.Preto), new PosicaoXadrez('a', 8).ToPosicao());
+        tab.colocarPeca(new Cavalo(tab, Cor.Preto), new PosicaoXadrez('b', 8).ToPosicao());
+        tab.colocarPeca(new Bispo(tab, Cor.Preto), new PosicaoXadrez('c', 8).ToPosicao());
+        tab.colocarPeca(new Rainha(tab, Cor.Preto), new PosicaoXadrez('d', 8).ToPosicao());
+        tab.colocarPeca(new Rei(tab, Cor.Preto), new PosicaoXadrez('e', 8).ToPosicao());
+        tab.colocarPeca(new Bispo(tab, Cor.Preto), new PosicaoXadrez('f', 8).ToPosicao());
+        tab.colocarPeca(new Cavalo(tab, Cor.Preto), new PosicaoXadrez('g', 8).ToPosicao());
+        tab.colocarPeca(new Torre(tab, Cor.Preto), new PosicaoXadrez('h', 8).ToPosicao());
         //BRANCAS
-        tab.Colocarpeca(new Torre(tab, Cor.Branco), new PosicaoXadrez('a', 1).ToPosicao());
-        tab.Colocarpeca(new Cavalo(tab, Cor.Branco), new PosicaoXadrez('b', 1).ToPosicao());
-        tab.Colocarpeca(new Bispo(tab, Cor.Branco), new PosicaoXadrez('c', 1).ToPosicao());
-        tab.Colocarpeca(new Rei(tab, Cor.Branco), new PosicaoXadrez('d', 1).ToPosicao());
-        tab.Colocarpeca(new Rainha(tab, Cor.Branco), new PosicaoXadrez('e', 1).ToPosicao());
-        tab.Colocarpeca(new Bispo(tab, Cor.Branco), new PosicaoXadrez('f', 1).ToPosicao());
-        tab.Colocarpeca(new Cavalo(tab, Cor.Branco), new PosicaoXadrez('g', 1).ToPosicao());
-        tab.Colocarpeca(new Torre(tab, Cor.Branco), new PosicaoXadrez('h', 1).ToPosicao());
+        tab.colocarPeca(new Torre(tab, Cor.Branco), new PosicaoXadrez('a', 1).ToPosicao());
+        tab.colocarPeca(new Cavalo(tab, Cor.Branco), new PosicaoXadrez('b', 1).ToPosicao());
+        tab.colocarPeca(new Bispo(tab, Cor.Branco), new PosicaoXadrez('c', 1).ToPosicao());
+        tab.colocarPeca(new Rei(tab, Cor.Branco), new PosicaoXadrez('d', 1).ToPosicao());
+        tab.colocarPeca(new Rainha(tab, Cor.Branco), new PosicaoXadrez('e', 1).ToPosicao());
+        tab.colocarPeca(new Bispo(tab, Cor.Branco), new PosicaoXadrez('f', 1).ToPosicao());
+        tab.colocarPeca(new Cavalo(tab, Cor.Branco), new PosicaoXadrez('g', 1).ToPosicao());
+        tab.colocarPeca(new Torre(tab, Cor.Branco), new PosicaoXadrez('h', 1).ToPosicao());
         Tela.imprimirxadrezdelay(tab);
 
     }
--- Xadrez/Tabuleiro/Tabuleiro.cs
+++ Xadrez/Tabuleiro/Tabuleiro.cs
@@ -18,10 +18,11 @@
         public Peca peca(int linha, int coluna) {
             return pecas[linha, coluna];
         }
-        public void Colocarpeca(Peca p, Posicao pos) {
+        public void colocarPeca(Peca p, Posicao pos) {
             if(existepeca(pos)){
                 throw new TabuleiroException("Ja existe uma peça nessa posiçao");
             }
+
             pecas[pos.Linha, pos.Coluna] = p;
             p.posicao = pos;
       
@@ -34,24 +35,30 @@
         }
         
 
-        public Peca retirarpeca(Posicao pos) {
+        public Peca retirarPeca(Posicao pos) {
             if (peca(pos) == null) {
                 return null;
             }
+
             Peca aux = peca(pos);
             aux.posicao = null;
             pecas[pos.Linha, pos.Coluna] = null;
             return aux;
         }
-        public Peca MoverPeca(Peca p,Posicao initpos,Posicao endpos) {
-            Peca p1;
-            if (existepeca(endpos) && posicaovalida(endpos)) {
-                p1 = retirarpeca(endpos);
-                Colocarpeca(p, endpos);
-                retirarpeca(initpos);
-                return p1;
-            }
+        public Peca MoverPeca(Posicao initPos,Posicao endPos) {
             
+            if (posicaovalida(endPos)) {
+                Peca p1;
+                if(peca(endPos)!=null){
+                    if(peca(initPos).cor==peca(endPos).cor){
+                    throw new TabuleiroException("O local onde a peça esta tentando se mover é ocupado por uma peca da mesma cor");
+                    }
+                }    
+                p1 = retirarPeca(endPos);
+                colocarPeca(peca(initPos),endPos);
+                retirarPeca(initPos);
+                return p1;
+            }          
             return null;
         }
 
